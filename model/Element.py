6f7fafb433d5bf7b0577aa5e651b366b23c6f0e9
serenasensini <serena.sensini@gmail.com>
2018-06-17 12:16:55 +0200

    modified models

--- model/Element.py
+++ model/Element.py
@@ -1,8 +1,8 @@
 class Element():
 	
-	def __init__(self, properties):
-		self.properties = properties
-		self.has_evaluation = False
+	def __init__(self, has_property):
+		self.id = id
+		self.has_property = has_property
 		
 	def __repr__(self):
 		print ''
---
+++ model/User.py
@@ -0,0 +1,8 @@
+class User():
+
+    def __init__(self, id, error_rate):
+        self.id = id
+        self.error_rate = error_rate
+
+    def __repr__(self):
+        pass
