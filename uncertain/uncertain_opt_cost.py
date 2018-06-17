9eb09430657302d8b60d307229da4ce7ffbb71b4
serenasensini <serena.sensini@gmail.com>
2018-06-17 11:20:24 +0200

    defined evaluate_item and uncOptCost functions

--- uncertain/uncertain_opt_cost.py
+++ uncertain/uncertain_opt_cost.py
@@ -2,42 +2,30 @@
 # -*- coding: iso-8859-15 -*-
 
 probability_threshold = 0.6
+cats_items = {}
+junk_items = {}
+doubt_items = {}
 
-def uncertain_opt_cost(items,K1,strategy):
+def uncOptCost(items, K1, strategy):
+    while len(cats_items) < K1:
+        for item in items:
+            #missing answers
+            evaluate_item(item, answers, strategy)
+            if len(cats_items) == K1:
+                break
 
-    n_plus, n_min = 0
-    items_copy = items
-    answers = []
-    current_questions = []
-    while len(answers) < K1:
-        current_element= calculate(items_copy[0:K1-len(answers)])
+def evaluate_item(item, answers, strategy):
+    #for each assistant, evaluate the item and store the answer in a list
+    #the answers contained an error rate yet
+    result = threshold(answers)
+    #there's a cat
+    if result == 1:
+        cats_items.append(item)
+    elif result == -1:
+        junk_items.append(item)
+    elif result == 0:
+        doubt_items.append(item)
 
-        #sbagliato da rivedere
-
-        for i in current_element:
-            if calculate(items) == 0:
-                n_plus = n_plus + 1
-            else:
-                n_min = n_min + 1
-            # y(r(i) = y(0,0)
-            current_questions.append(min(n_min,n_plus))
-
-        answers = input()
-        for i in current_questions:
-            if strategy(answers) == 1:
-                del (items_copy[i])
-                set.append(i)
-
-            if strategy(answers) == 0:
-                del (items_copy[i])
-
-
-def calculate(items):
-    cost = []
-    for item in items:
-        temp = probability.item * n_questions
-        cost.append(temp)
-    return min(cost)
 
 #calculating y probability
 def expected_cost(pos_questions, neg_questions, tot_questions):
